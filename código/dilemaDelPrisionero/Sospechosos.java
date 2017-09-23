c612349e79d9a8fb579fca1be1e8c4296919923c
jamesmememes <32228445+jamesmememes@users.noreply.github.com>
2017-09-23 15:56:26 -0600

    creación de la primera clase
    Sospechosos 
    1. El agente egoísta que siempre Confiesa con el fin de obtener 0 años de cárcel.
    2. El agente ingenuo que siempre opta por No Confesar.
    3. El agente aleatorio selecciona al azar en cada momento si esa vez confiesa o no.
    4. Un agente imitador que hace lo que su rival hizo en el turno anterior. Al inicio decide al azar que
    hacer.
    5. Un agente imitador con traición que al inicio No Confiesa, pero tiene un 90% de probabilidad de
    hacer lo mismo que su rival en el turno anterior, pero tiene una probabilidad del 10% de Confesar.
    6. Un agente original cuya estrategia ustedes definirán tratando de obtener la menor cantidad promedio
    de años de carcel si se enfrenta con agentes de los otros tipos.

---
+++ código/dilemaDelPrisionero/Sospechosos.java
@@ -0,0 +1,30 @@
+public class Sospechosos {
+ 
+    
+ public String egoista () { //siempre confiesa, busca 0 años 
+  String respuesta=""; 
+  return respuesta;      
+ } 
+ 
+ public String ingenuo () { // nunca confiesa
+ String respuesta="";    
+ return respuesta; 
+ }
+ 
+ public String aleatorio () { // siempre es al azar
+  String respuesta ="";
+  return respuesta; 
+ }
+ 
+ public String imitador () { // copia lo que hizo su rival en el turno anterior, primer turno aleatorio
+  String respuesta=""; 
+  return respuesta;     
+ }
+ 
+ public String agenteOrinigal () { // nosotros lo creamos 
+  String respuesta =""; 
+  return respuesta;    
+ }
+ 
+ 
+}
