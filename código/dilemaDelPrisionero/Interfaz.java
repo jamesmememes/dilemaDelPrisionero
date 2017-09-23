5c24f5bb0e0686de358a68a6099511f2a882b21a
Isacc <jamesmememescr@gmail.com>
2017-09-23 17:38:52 -0600

    Interfaz
    se comenzo a dar forma a la interfaz JOptionPane, no estoy seguro si vamos a necesitar todos los metodos que cree en la interfaz.

--- código/dilemaDelPrisionero/Interfaz.java
+++ código/dilemaDelPrisionero/Interfaz.java
@@ -1,6 +1,22 @@
+import javax.swing.JOptionPane; 
+
 public class Interfaz {
 
- public Interfaz () {
-    
+ public void mostrarString (String mensaje, String titulo) {  // esto si lo vamos a necesitar 
+  JOptionPane.showMessageDialog(null, mensaje, titulo, JOptionPane.PLAIN_MESSAGE);  
+ }
+ 
+ 
+   // DE AQUÍ NOSE SI VAMOS A NECESITAR SOLICITAR ALGÚN TIPO DE DATO AL USUARIO 
+ public String solicitarString (String mensaje) {
+  return JOptionPane.showInputDialog(mensaje); 
+ }
+ 
+ public double solicitarDouble (String mensaje){
+  return Double.parseDouble(JOptionPane.showInputDialog(mensaje));   
+ }
+ 
+ public int solicitarInt (String mensaje) {
+  return Integer.parseInt(JOptionPane.showInputDialog(mensaje));    
  }
 }
