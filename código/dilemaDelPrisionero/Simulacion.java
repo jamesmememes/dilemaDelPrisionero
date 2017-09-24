84c682df7a2e4b2a63d1561f1a03bf5e79559579
Isacc <jamesmememescr@gmail.com>
2017-09-24 16:24:44 -0600

    Simulacion
    estructura

--- código/dilemaDelPrisionero/Simulacion.java
+++ código/dilemaDelPrisionero/Simulacion.java
@@ -2,10 +2,31 @@ public class Simulacion {
  Sospechosos enfrentamientos = new Sospechosos (); 
  
  public Simulacion () {
-  String condena = ""; 
-  if (enfrentamientos.aleatorio.decision == enfrentamiento.ingenuo.decision) {
+  int condenaSospechoso1 = 0;
+  int condenaSospechoso2 = 0; 
+  if (enfrentamientos.aleatorio.decision == enfrentamientos.ingenuo.decision) {
+   if (enfrentamientos.aleatorio.decision == "confieso") {
+    enfrentamientos.aleatorio.anyosDeCarcel = (condenaSospechoso1 =  6);
+    enfrentamientos.ingenuo.anyosDeCarcel = (condenaSospechoso2 =  6);
+   }
+   
+   else {
+    enfrentamientos.aleatorio.anyosDeCarcel = (condenaSospechoso1 =  1);
+    enfrentamientos.ingenuo.anyosDeCarcel = (condenaSospechoso2 =  1);  
+   }
+   }
+   
+  else {
+    if (enfrentamientos.aleatorio.decision == "confieso") {
+     enfrentamientos.aleatorio.anyosDeCarcel = (condenaSospechoso1 = 0);
+     enfrentamientos.ingenuo.anyosDeCarcel = (condenaSospechoso2 = 10); 
+    }
     
-  
+    else {
+     enfrentamientos.aleatorio.anyosDeCarcel = (condenaSospechoso1 = 10);
+     enfrentamientos.ingenuo.anyosDeCarcel = (condenaSospechoso2 = 0);
+    }
+      
   }; 
     
  }
