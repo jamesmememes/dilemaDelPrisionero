b01882aa49fc77a0bac6483d0a020731924ce363
Isacc <jamesmememescr@gmail.com>
2017-09-23 18:13:04 -0600

    interfaz
    consola

--- código/dilemaDelPrisionero/Interfaz.java
+++ código/dilemaDelPrisionero/Interfaz.java
@@ -1,7 +1,11 @@
 import javax.swing.JOptionPane; 
+import java.io.*;
+import java.io.InputStreamReader; 
+import java.io.BufferedReader; 
 
 public class Interfaz {
 
+        // JOptionPANE
  public void mostrarString (String mensaje, String titulo) {  // esto si lo vamos a necesitar 
   JOptionPane.showMessageDialog(null, mensaje, titulo, JOptionPane.PLAIN_MESSAGE);  
  }
@@ -18,5 +22,14 @@ public class Interfaz {
  
  public int solicitarInt (String mensaje) {
   return Integer.parseInt(JOptionPane.showInputDialog(mensaje));    
+ }
+ 
+       //CONSOLA 
+       
+ InputStreamReader isr = new InputStreamReader(System.in);  // se crea un flujo de entrada de lo que pasa en el sistema
+ BufferedReader br = new BufferedReader(isr); // almacenamos lo que estamos leyendo que proviene de isr
+ public String solocitarString () {
+  
+ return   
  }
 }
