3f27f29894e27f67a79c88a054aefddaef32a8ff
Isacc <jamesmememescr@gmail.com>
2017-09-24 05:48:21 -0600

    Rondas
    nueva clase

---
+++ código/dilemaDelPrisionero/Rondas.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Rondas
+{
+    public void rondas(){
+        Scanner input = new Scanner(System.in);
+        
+        int numrondas = input.nextInt();
+        
+    }
+    
+
+}
