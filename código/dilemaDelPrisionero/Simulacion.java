aed958e97e07b134a243bd82093c84b4042289ab
Isacc <jamesmememescr@gmail.com>
2017-09-24 16:35:46 -0600

    solucion
    no me reconocia los metodos de la clase sospechosos, entonces le puse el "( )" despues de cada metodo, y creo que funciona.
    NOTE:  pero ahora no me reconoce las variables de los metodos.

--- código/dilemaDelPrisionero/Simulacion.java
+++ código/dilemaDelPrisionero/Simulacion.java
@@ -4,27 +4,27 @@ public class Simulacion {
  public Simulacion () {
   int condenaSospechoso1 = 0;
   int condenaSospechoso2 = 0; 
-  if (enfrentamientos.aleatorio.decision == enfrentamientos.ingenuo.decision) {
-   if (enfrentamientos.aleatorio.decision == "confieso") {
-    enfrentamientos.aleatorio.anyosDeCarcel = (condenaSospechoso1 =  6);
-    enfrentamientos.ingenuo.anyosDeCarcel = (condenaSospechoso2 =  6);
+  if (enfrentamientos.aleatorio().decision == enfrentamientos.ingenuo().decision) {
+   if (enfrentamientos.aleatorio().decision == "confieso") {
+    enfrentamientos.aleatorio().anyosDeCarcel = (condenaSospechoso1 =  6);
+    enfrentamientos.ingenuo().anyosDeCarcel = (condenaSospechoso2 =  6);
    }
    
    else {
-    enfrentamientos.aleatorio.anyosDeCarcel = (condenaSospechoso1 =  1);
-    enfrentamientos.ingenuo.anyosDeCarcel = (condenaSospechoso2 =  1);  
+    enfrentamientos.aleatorio().anyosDeCarcel = (condenaSospechoso1 =  1);
+    enfrentamientos.ingenuo().anyosDeCarcel = (condenaSospechoso2 =  1);  
    }
    }
    
   else {
-    if (enfrentamientos.aleatorio.decision == "confieso") {
-     enfrentamientos.aleatorio.anyosDeCarcel = (condenaSospechoso1 = 0);
-     enfrentamientos.ingenuo.anyosDeCarcel = (condenaSospechoso2 = 10); 
+    if (enfrentamientos.aleatorio().decision == "confieso") {
+     enfrentamientos.aleatorio().anyosDeCarcel = (condenaSospechoso1 = 0);
+     enfrentamientos.ingenuo().anyosDeCarcel = (condenaSospechoso2 = 10); 
     }
     
     else {
-     enfrentamientos.aleatorio.anyosDeCarcel = (condenaSospechoso1 = 10);
-     enfrentamientos.ingenuo.anyosDeCarcel = (condenaSospechoso2 = 0);
+     enfrentamientos.aleatorio().anyosDeCarcel = (condenaSospechoso1 = 10);
+     enfrentamientos.ingenuo().anyosDeCarcel = (condenaSospechoso2 = 0);
     }
       
   }; 
