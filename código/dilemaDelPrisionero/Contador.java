37fefb2380779c25ec5cbc9e6ba1da0f70cb2333
yulianloaiza <32231736+yulianloaiza@users.noreply.github.com>
2017-09-23 21:40:26 -0600

    Create Contador.java

---
+++ código/dilemaDelPrisionero/Contador.java
@@ -0,0 +1,53 @@
+
+public class contador extends Sospechosos
+{
+        /**
+        *Clase de contador, que presenta todos los resultados que se piden de los sospechosos
+        *MODIFICAR o hacer cualquier cambio para que funcione bien :b
+        */
+
+        // Asumiendo que estamos en modo EXPERIMENTO GRUPAL           BTW, ¿Como vamos a diferenciar experimento grupal de especifico?
+    
+    boolean grupal = true;
+    public void totalanyos (){
+        if (grupal = true){
+            /**Hacemos en la clase un metodo que cuente los años de carcel
+             * de cada sospechoso;  lo llamamos aca y hacemos la mate
+             */
+            total = egoista.anyos + ingenuo.anyos + aleatorio.anyos + imitador.anyos;
+            
+            int maxValue = 0;
+            int minValue = 800000
+            
+            /**Hacer un metodo con recursividad para que los anyos de los agentes los 
+             * vea, y vaya 1 por 1 verificando si es menor o mayo que el valor actual
+             */
+            if(METODO RECURSIVO>maxValue){
+                
+                maxValue = METODO RECURSIVO; 
+            }
+            //Asi tendriamos el numero mayor y abajo el menor, para comparar
+            
+            if (METODO RECURSIVO<minValue){
+                
+                minValue = METODO RECURSIVO;
+            }
+            
+            System.out.println:"El anyo en conjunto de todos los sospechosos fue" + total;
+            System.out.println:"Anyos de agente egoista:" + anyos.egoista
+            /** System.out.println:"El agente que fue mas anyos fue " HACER METODO
+             * PARA QUE DIGA EL NOMBRE DEL SOSPECHOSO CON MENOS ANYOS+ "con " +
+             * maxValue + "anyos" 
+            */
+           
+            /**System.out.println:"El agente que fue menos anyos fue " HACER METODO
+             * PARA QUE DIGA EL NOMBRE DEL SOSPECHOSO CON MAS ANYOS+ "con " +
+             * minValue + "anyos" 
+             */
+   
+          
+    
+      
+        }
+    }      
+}
