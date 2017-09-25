f02ac337e09cb18ac23bc20d7ce8a0fd3585ec0b
Isacc <jamesmememescr@gmail.com>
2017-09-24 18:34:26 -0600

    Sospechosos
    agregue mas codigo para el imitador

--- código/dilemaDelPrisionero/Sospechosos.java
+++ código/dilemaDelPrisionero/Sospechosos.java
@@ -31,9 +31,26 @@ public class Sospechosos {
  }
  
  public String imitador () { // copia lo que hizo su rival en el turno anterior, primer turno aleatorio
+  int banderaDeInicio = 0; 
   int anyosDeCarcel = 0;
   String decision=""; 
-  return decision;     
+  
+  if (banderaDeInicio == 0) {  // como la primera ronda es aleatorio puse un contador = 0; y automaticamente ingresa le sumo 1
+   int numeroAleatorio = (int)(Math.random()*2); // por lo que la siguiente ronda no va a entrar a este if y va pasar directo al else. 
+      ++banderaDeInicio;                       // en el else va tener que estar el código para copiar el turno del oponente. 
+     if (numeroAleatorio == 0) { // número aleatorio se utiliza para generar una desicion randon de 1/2 posibilidades, 1 ó 0
+        decision = "confieso";   
+     }  
+  
+     else {
+        decision = "no confieso"; 
+     }
+  }
+  
+  else {
+    
+  }
+  return decision;   
  }
  
  public String agenteOriginal () { // estratégia de obtener menor cantidad de años en prisión
